51f7511173c76434812ee657d13ccb51fe225396
georgesebastiansanchez <george3212378060@gmail.com>
2026-03-18 15:04:49 -0500

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Esto permite que cualquier dominio (incluyendo tu Railway) se conecte
+    'allowed_origins' => ['*'], 
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], 
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false, // Cámbialo a false si no usas cookies/Sanctum para evitar conflictos
+    'supports_credentials' => false,
 ];
