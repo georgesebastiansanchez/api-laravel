cfa2a4cbca376b8bcb570be69462a8bebe9d5ada
georgesebastiansanchez <george3212378060@gmail.com>
2026-03-18 15:45:45 -0500

    Restrict allowed origins for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], 
+    'allowed_origins' => ['https://react-production-ca03.up.railway.app'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], 
     'exposed_headers' => [],
