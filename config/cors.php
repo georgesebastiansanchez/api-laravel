1641a5e2e852f3c0a64868f8252e6ec7fabd2822
georgesebastiansanchez <george3212378060@gmail.com>
2026-03-18 17:31:47 -0500

    Fix formatting in cors.php for allowed_headers

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://react-production-ca03.up.railway.app'], 
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'], .
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
