66b6ecdf6fb0173e9cf519868638034c9b2a4b33
georgesebastiansanchez <george3212378060@gmail.com>
2026-03-18 20:46:56 -0500

    Update allowed_origins to use environment variable

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://react-production-ca03.up.railway.app'], 
+    'allowed_origins' => [env('FRONTEND_URL', '*')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
