c4314cc5fac4fc5ffab4adbb93c68f1087b0ed97
georgesebastiansanchez <george3212378060@gmail.com>
2026-03-26 23:00:20 -0500

    Update CORS configuration to allow localhost
    Added localhost support for CORS origins and patterns.

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', '*')],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [
+    env('FRONTEND_URL', '*'),
+    'http://localhost:*',
+],
+    'allowed_origins_patterns' => ['#^http://localhost(:\d+)?$#'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
